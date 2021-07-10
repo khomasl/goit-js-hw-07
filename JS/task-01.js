83435d6439929c7c6cc08b7d4dc52593a7386189
khomasl <gymnasium.yv@gmail.com>
2021-07-10 13:39:27 +0300

    Коментарий к 1-му

--- JS/task-01.js
+++ JS/task-01.js
@@ -6,6 +6,7 @@ const arrCategories = document.querySelectorAll('li.item');
 console.log(`В списке ${arrCategories.length} категории.`);
 
 // Виводить контент всіх заголовків h2 в списку
+// і к-ть елементів списку кожної з категорій
 const arrChildren = [...arrCategories].map(elem => elem.children);
 arrChildren.forEach(elem => 
     console.log(` Категория: ${elem[0].textContent} \n Количество элементов: ${elem[1].childElementCount}`));
